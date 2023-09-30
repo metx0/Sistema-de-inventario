58564b9a3c7bc56cf3f1e116eb372a05ec82df81
Antonorz <131824060+Antonorz@users.noreply.github.com>
2023-09-30 10:54:42 -0600

    Update CreadorCSV.java
    Se agrego un File y FileWriter.
    Se agrego la capacidad de agregar un nuevo archivo en caso de que exista uno con el mismo nombre.

--- CreadorCSV.java
+++ CreadorCSV.java
@@ -1,7 +1,5 @@
-
-package creadorcsv;
-
-import com.csvreader.CsvWriter; //https://www.csvreader.com/java_csv.php
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 
@@ -11,30 +9,54 @@ import java.util.ArrayList;
  * @author anton
  */
 public class CreadorCSV {
+    
+    //Detecta si existe un archivo con nombreArchivo (el mismo de nombreArchivoBase)
+    public static boolean archivoExiste(String nombreArchivo) {
+        File archivo = new File(nombreArchivo);
+        return archivo.exists();
+    }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) throws IOException {
-                // Crear un ArrayList de productos
+        // Crear un ArrayList de productos
         ArrayList<Producto> productos = new ArrayList<>();
 
-        // Agregar productos al ArrayList
+        /* Agregar productos al ArrayList, en esta parte desde la interfaz se pueden añadir
+        productos con add, aca un ejemplo
         productos.add(new Producto("Producto A", 1, 50, "26-09-2023"));
         productos.add(new Producto("Producto B", 2, 134.50, "27-09-2023"));
         productos.add(new Producto("Producto C", 3, 12.99, "28-09-2023"));
         productos.add(new Producto("Producto D", 4, 345, "29-09-2023"));
+        */
+        
+        // Nombre base del archivo
+        String nombreArchivoBase = "productos_testeo.csv";
+        String nombreArchivo = nombreArchivoBase;
+        int contador = 1;
 
-        // Crear un objeto CsvWriter
-        CsvWriter csvWriter = new CsvWriter("productos.csv");
+        // Verifica si un archivo existe, si ya hay uno asi, se le agrega un numero 
+        while (archivoExiste(nombreArchivo)) {
+            nombreArchivo = nombreArchivoBase.replace(".csv", "_" + contador + ".csv");
+            contador++;
+        }
+
+        
+        //Crea un FileWriter para un archivo CSV
+        FileWriter creador_csv = new FileWriter(nombreArchivoBase);       
 
         // Escribir los datos de los productos en el archivo CSV
         for (Producto producto : productos) {
-            csvWriter.writeRecord(producto.getArray());
+            //Une los elementos con comas
+            String lineaCSV = String.join(",", producto.getArray()); 
+            //Utiliza write para escribir la línea
+            creador_csv.write(lineaCSV);
+            //Agrega un salto de linea después de cada línea
+            creador_csv.write(System.lineSeparator());
         }
 
-        // Cerrar el objeto CsvWriter
-        csvWriter.close();
-    }
-    
+        // Cerrar el objeto FileWriter
+        creador_csv.close();
+    }   
 }
